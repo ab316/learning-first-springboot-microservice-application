77e38b77732b2b844b8d0f60011563a733466113
Abdullah Baig <abdullahbaig456@gmail.com>
2019-11-14 20:48:52 +0100

    Gallery service: Add Sleuth for tracing

--- gallery-service/src/main/java/com/learning/gallery/rest/HomeController.java
+++ gallery-service/src/main/java/com/learning/gallery/rest/HomeController.java
@@ -2,6 +2,8 @@ package com.learning.gallery.rest;
 
 import com.learning.gallery.model.Gallery;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private RestTemplate restTemplate;
 
@@ -29,12 +32,15 @@ public class HomeController {
     @HystrixCommand(fallbackMethod = "fallback")
     @GetMapping(value = "/gallery/{id}", produces = "application/json")
     public Gallery getGallery(@PathVariable final int id) {
+        LOGGER.info("Creating gallery object");
         var gallery = new Gallery();
         gallery.setId(id);
 
         @SuppressWarnings("unchecked")
         List<Object> images = restTemplate.getForObject("http://image-service/images", List.class);
         gallery.setImages(images);
+        
+        LOGGER.info("Returning images");
         return gallery;
     }
 
@@ -44,6 +50,7 @@ public class HomeController {
     }
 
     public Gallery fallback(int galleryId, Throwable hysterixCommand) {
+        LOGGER.warn("Fallback method invoked");
         return new Gallery(galleryId);
     }
 }
